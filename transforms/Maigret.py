2530d956abfeb73476a1de193641e5535158ddcf
Soxoj <soxoj@protonmail.com>
2021-12-08 23:02:17 +0300

    Updated transformation code

--- transforms/Maigret.py
+++ transforms/Maigret.py
@@ -1,73 +1,52 @@
 import asyncio
 import logging
 from mock import Mock
+import sys
 
 from maltego_trx.entities import Alias, URL
-
 from maltego_trx.transform import DiscoverableTransform
 from maltego_trx.maltego import LINK_STYLE_DASHED
 
-from maigret.maigret import maigret
+import maigret
 from maigret.result import QueryStatus
-from maigret.sites import MaigretDatabase
 
-MAIGRET_DB_URL = 'https://raw.githubusercontent.com/soxoj/maigret/main/maigret/resources/data.json'
-DB = MaigretDatabase().load_from_url(MAIGRET_DB_URL)
+logging.getLogger('asyncio').setLevel(logging.WARNING)
+
+
+MAIGRET_DB_FILE = 'https://raw.githubusercontent.com/soxoj/maigret/main/maigret/resources/data.json'
 TOP_SITES_COUNT = 500
-TIMEOUT = 5
+TIMEOUT = 10
 
 
-PROPERTIES = {
-    'SocialNetwork': {
-        'label': 'Social Network',
-    },
-    'URL': {
-        'label': 'URL',
-    }
-}
+def setup_logger(log_level, name):
+    for lib in ['requests', 'urllib3']:
+        logging.getLogger(lib).setLevel(logging.WARNING) # change to debug
+    logger = logging.getLogger(name)
+    logger.setLevel(log_level)
+    return logger
 
 
 async def maigret_search(username):
     """
         Main Maigret search function
     """
-    global DB
+    logger = setup_logger(logging.ERROR, 'maigret')
+    db = maigret.MaigretDatabase().load_from_path(MAIGRET_DB_FILE)
+    sites = db.ranked_sites_dict(top=TOP_SITES_COUNT)
 
-    log_level = logging.WARNING
-    logging.basicConfig(
-        format='[%(filename)s:%(lineno)d] %(levelname)-3s  %(asctime)s %(message)s',
-        datefmt='%H:%M:%S',
-        level=log_level
-    )
-    logger = logging.getLogger('maigret')
-    logger.setLevel(log_level)
-
-    # workaround to update database from git master
-    try:
-        db = MaigretDatabase().load_from_url(MAIGRET_DB_URL)
-        DB = db
-    except Exception as e:
-        logger.error(e)
-        db = DB
-
-    site_data = db.ranked_sites_dict(top=TOP_SITES_COUNT)
-    # can be object with editing tg msg handler to show search progress
-    query_notify = Mock()
-
-    results = await maigret(username,
-                            dict(site_data),
-                            query_notify,
-                            timeout=TIMEOUT,
-                            logger=logger,
-                            no_progressbar=True,
-                            recursive_search=True,
-                            )
+    results = await maigret.search(username=username,
+                                   site_dict=sites,
+                                   timeout=TIMEOUT,
+                                   logger=logger,
+                                   no_progressbar=True, # change to debug
+                                   query_notify=None, # change to debug
+                                   )
     return results
 
 
 class Maigret(DiscoverableTransform):
     """
-    Returns aliases for .
+    Returns aliases for the input alias entity
     """
 
     @classmethod
@@ -76,14 +55,18 @@ class Maigret(DiscoverableTransform):
 
         for site, data in Maigret.get_maigret_data(person_name).items():
             url_main = data['url_main']
+            url_user = data['url_user']
 
-            entity = response.addEntity(URL, url_main)
+            entity = response.addEntity(Alias, site)
             entity.setLinkLabel('found by username')
-            entity.addDisplayInformation(content='hello')
+            entity.addDisplayInformation(content=f'{person_name} profile at {site}')
             # entity.setWeight(100)
             entity.setIconURL('https://www.google.com/s2/favicons?domain='+url_main)
-            for key in ['url', 'Social Network', 'Title']:
-                entity.addProperty(key.replace(' ', ''), key, 'strict', url_main)
+
+            entity.addProperty('Title', 'Title', 'strict', site)
+            entity.addProperty('Social Network', 'SocialNetwork', 'strict', site)
+
+            entity.addProperty('Url', 'Url', 'strict', url_user)
 
     @staticmethod
     def get_maigret_data(username):
@@ -92,8 +75,10 @@ class Maigret(DiscoverableTransform):
 
         res = {}
         for site, data in results.items():
-            if data['status'].status == QueryStatus.CLAIMED:
-                res[site] = data
+            if data['status'].status != QueryStatus.CLAIMED:
+                continue
+
+            res[site] = data
 
         return res
 
---
+++ transforms/__init__.py
@@ -0,0 +1,1 @@
+from .Maigret import Maigret
